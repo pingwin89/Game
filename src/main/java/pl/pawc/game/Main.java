18e904a63f69259aa2ba5ac7936139bf039ef100
pawc <pswid89@gmail.com>
2017-08-08 20:36:08 +0200

    animation

--- src/main/java/pl/pawc/game/Main.java
+++ src/main/java/pl/pawc/game/Main.java
@@ -1,15 +1,18 @@
 package pl.pawc.game;
 
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.LoopMode;
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.DirectionalLight;
-import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
+import com.jme3.scene.Node;
 
 public class Main extends SimpleApplication{
+	
+	private AnimControl control;
+	private AnimChannel channel;
 	
     public static void main(String[] args){
     	 Main app = new Main();
@@ -18,12 +21,24 @@ public class Main extends SimpleApplication{
 
 	@Override
 	public void simpleInitApp(){
+		
+		viewPort.setBackgroundColor(ColorRGBA.Blue);
+		flyCam.setMoveSpeed(50);
 		       
 		DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
+        sun.setDirection(new Vector3f(-1.0f, -1.0f, -1.0f));
         rootNode.addLight(sun);
-        
-		Spatial dog = assetManager.loadModel("assets/Dog/Dog.j3o");	
+
+		Node dog = (Node) assetManager.loadModel("assets/Dog/Dog.j3o");	
+		
+		Node armature = (Node) dog.getChild("Armature");
+	    Node Dog = (Node) armature.getChild(("Dog"));
+	    control = Dog.getControl(AnimControl.class);
+		
+	    channel = control.createChannel();
+	    channel.setAnim("ArmatureAction");
+	    channel.setLoopMode(LoopMode.Loop);
+	    
         rootNode.attachChild(dog);
 	
 	}
